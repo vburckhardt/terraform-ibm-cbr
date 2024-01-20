1145c0cc1e91ae80d0266f422c619ecf0e14b820
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2024-01-20 12:04:14 +0000

    chore(deps): update terraform-module (#389)

--- examples/multi-resource-rule/main.tf
+++ examples/multi-resource-rule/main.tf
@@ -42,7 +42,7 @@ data "ibm_iam_account_settings" "iam_account_settings" {
 
 module "cbr_zone_vpc" {
   source           = "terraform-ibm-modules/cbr/ibm//modules/cbr-zone-module"
-  version          = "1.17.1"
+  version          = "1.18.0"
   name             = "${var.prefix}-VPC-network-zone"
   zone_description = "CBR Network zone containing VPC"
   account_id       = data.ibm_iam_account_settings.iam_account_settings.account_id
@@ -54,7 +54,7 @@ module "cbr_zone_vpc" {
 
 module "cos_instance_and_bucket" {
   source                        = "terraform-ibm-modules/cos/ibm"
-  version                       = "7.1.4"
+  version                       = "7.1.5"
   resource_group_id             = module.resource_group.resource_group_id
   region                        = var.region
   create_cos_instance           = true
