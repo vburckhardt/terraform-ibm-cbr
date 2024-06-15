85bbca8ed3a151fa78bc8d0ca756710ca08a6cab
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2024-06-15 09:24:39 -0400

    chore(deps): update terraform-module (#460)

--- examples/multi-resource-rule/main.tf
+++ examples/multi-resource-rule/main.tf
@@ -4,7 +4,7 @@
 
 module "resource_group" {
   source  = "terraform-ibm-modules/resource-group/ibm"
-  version = "1.1.5"
+  version = "1.1.6"
   # if an existing resource group is not set (null) create a new one using prefix
   resource_group_name          = var.resource_group == null ? "${var.prefix}-resource-group" : null
   existing_resource_group_name = var.resource_group
@@ -53,7 +53,7 @@ module "cbr_zone_vpc" {
 
 module "cos_instance_and_bucket" {
   source                        = "terraform-ibm-modules/cos/ibm"
-  version                       = "8.3.0"
+  version                       = "8.4.1"
   resource_group_id             = module.resource_group.resource_group_id
   region                        = var.region
   create_cos_instance           = true
