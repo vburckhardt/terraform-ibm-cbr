00c5dfd2d8cc5890a4597f546f2e4eaca946f74f
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2024-10-20 10:57:36 +0100

    chore(deps): update terraform terraform-ibm-modules/key-protect/ibm to v2.8.6 (#553)

--- examples/fscloud/main.tf
+++ examples/fscloud/main.tf
@@ -15,7 +15,7 @@ module "resource_group" {
 ##############################################################################
 module "key_protect_module" {
   source            = "terraform-ibm-modules/key-protect/ibm"
-  version           = "v2.8.5"
+  version           = "v2.8.6"
   key_protect_name  = "${var.prefix}-key-protect-instance"
   resource_group_id = module.resource_group.resource_group_id
   region            = var.region
